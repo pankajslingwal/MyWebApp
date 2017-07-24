1c45d6034fddf3f00f5d5cf0a5325590b47f4aad
NodePractice <pankaj00ait@gmail.com>
2017-07-24 16:25:10 +0400

    changes start for new post for enroll form

--- controller/enroll.js
+++ controller/enroll.js
@@ -12,6 +12,15 @@ router.post('/', function (req, res, next) {
     //request service that takes body or header as JSON object
     res.send('Username: ' + req.body);
 
+    //Or use formidable to get all form object
+
+    //try to build this form from react JS
+    ///Validate form here
+
+
+    // if ok send data directly to couchbase to save
+
+
     res.render('subscribe', { pagetitle: 'Post Subscribe Page', heading: 'Post Subscribe to Emails' });
 });
 
