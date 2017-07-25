3f5d61bf4ed01aa3fc476208b846348bc8cd3191
pankajslingwal <pankajslingwal@gmail.com>
2017-07-25 23:32:09 +0400

    adding formidable

--- controller/enroll.js
+++ controller/enroll.js
@@ -1,5 +1,6 @@
 const EXPRESS = require('express')
 var router = EXPRESS.Router()
+var formidable = require('formidable');
 
 router.get('/', function (req, res, next) {
     res.render('subscribe', { pagetitle: 'Subscribe Page', heading: 'Subscribe to Emails' });
@@ -7,10 +8,25 @@ router.get('/', function (req, res, next) {
 
 
 router.post('/', function (req, res, next) {
+    var form = new formidable.IncomingForm();
     //Create model of input data from Form
 
     //request service that takes body or header as JSON object
-    res.send('Username: ' + req.body);
+    //res.send('Username: ' + req.body);
+    //Get json of post
+    //create service will take this member object
+    //varefy if not registered
+    //send response based on that
+    
+     form.parse(req, function(err, fields, files) {
+         Console.log(fields);
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('form data:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+    
+
+     
 
     res.render('subscribe', { pagetitle: 'Post Subscribe Page', heading: 'Post Subscribe to Emails' });
 });
